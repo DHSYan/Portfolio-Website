690c3423dd2f81155da6e3ecbb0d3b34146d7541
Ding-Han Sean Yan <sean@dhsy.me>
2025-07-06 08:35:58 -0700

    feat: checkpoint

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,1 +1,20 @@
-alert("hello");
+function loadAboutMe() {
+  const current = document.getElementById("sceneHome");
+  const next = document.getElementById("sceneAboutMe");
+
+  
+  setTimeout(() => {
+    current?.classList.remove("scene-enable");
+    current?.classList.add("scene-disable");
+    next?.classList.remove("scene-disable");
+    next?.classList.add("scene-enable");
+  }, 1000);
+}
+
+function main() {
+  document
+  .getElementById("aboutMeButton")
+    ?.addEventListener("click", loadAboutMe);
+}
+
+main();
